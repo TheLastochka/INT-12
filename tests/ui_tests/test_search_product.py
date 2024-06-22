99eda57c033d02037b605818b81085b51d0cd82d
TheLastochka <aidar_uzakov@mail.ru>
2024-06-22 19:33:45 -0400

    Start ui-tests dev. Target site not responding

--- tests/ui_tests/test_search_product.py
+++ tests/ui_tests/test_search_product.py
@@ -7,10 +7,17 @@ import config
 @allure.feature("UI Tests")
 @allure.story("Search Product")
 # @pytest.mark.parametrize("product", ["Macbook", "iPhone", "iPad"])
-def test_search_product(page: Page):
+@pytest.mark.parametrize("website", [
+    'https://www.google.com', 
+    'https://vk.ru', 
+    'http://automationexercise.com',
+    'https://ya.ru', 
+    
+    ])
+def test_search_product(page: Page, website: str):
     with allure.step("Open the website"):
         # print({website})
-        page.goto(config.url.DOMAIN)
+        page.goto(website)
     #     expect(page.get_by_role("heading", name="AutomationExercise")).to_be_visible()
 
     # with allure.step("Click on 'Products' button"):
