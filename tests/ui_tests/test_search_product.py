3ab83bb6b718872b661b830e3a8ff6bb81153500
TheLastochka <aidar_uzakov@mail.ru>
2024-06-23 19:34:47 +0300

    add ui-test: Search product

--- tests/ui_tests/test_search_product.py
+++ tests/ui_tests/test_search_product.py
@@ -1,27 +1,28 @@
 import allure
 import pytest
 from playwright.sync_api import Page, expect
-import re
 import config
 
 @allure.feature("UI Tests")
 @allure.story("Search Product")
-# @pytest.mark.parametrize("product", ["Macbook", "iPhone", "iPad"])
-@pytest.mark.parametrize("website", [
-    'https://www.google.com', 
-    'https://vk.ru', 
-    'http://automationexercise.com',
-    'https://ya.ru', 
-    
+@pytest.mark.parametrize("product", [
+    "Men Tshirt",
+    "Frozen Tops For Kids",
+    "Soft Stretch Jeans"
     ])
-def test_search_product(page: Page, website: str):
+def test_search_product(page: Page, product: str):
     with allure.step("Open the website"):
-        # print({website})
-        page.goto(website)
-    #     expect(page.get_by_role("heading", name="AutomationExercise")).to_be_visible()
+        page.goto(config.url.DOMAIN)
+        expect(page.get_by_role("heading", name="AutomationExercise")).to_be_visible()
 
-    # with allure.step("Click on 'Products' button"):
-    #     page.get_by_role("button", name="Products").click()
-    #     expect(page.get_by_role("heading", name="Products")).to_be_visible()
-    
-    assert 1 == 1, "Test failed"
+    with allure.step("Click on 'Products' button"):
+        expect(page.get_by_role("link", name="Products"), "should be visible").to_be_visible()
+        page.get_by_role("link", name="Products").click()
+        expect(page.get_by_role("heading", name="ALL PRODUCTS")).to_be_visible()
+
+    with allure.step(f"Search for a product '{product}'"):
+        page.fill("#search_product", product)
+        page.click("#submit_search")
+
+    with allure.step("Verify search results"):
+        expect(page.locator(".single-products", has_text=product)).to_be_visible()
